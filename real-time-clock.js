2d1ac9c8fcb826fcb05b2821ff136c3ebb177bba
Dave-Rushabh <93786802+Dave-Rushabh@users.noreply.github.com>
2021-11-09 22:41:20 +0530

    Update real-time-clock.js

--- real-time-clock.js
+++ real-time-clock.js
@@ -1,11 +1,6 @@
 
 
-        // function clicked_1() {
-
-        //     console.log('This is done');
-        // }
-
-        let dd;
+       let dd;
         let time_time;
         let date_date;
         
@@ -15,12 +10,51 @@
             
         dd = new Date();
 
-        time_time = dd.getHours() + ':' + dd.getMinutes() + ':' + dd.getSeconds();
+        
 
-        date_date = dd.toLocaleDateString();
+        time_h = dd.getHours();
+        time_m = dd.getMinutes();
+        time_s = dd.getSeconds();
+
+        date_day = dd.getDate();
+        date_month = dd.getMonth();
+        date_year = dd.getFullYear();
 
         day_day = dd.getDay();
 
+        if (time_h <= 9){
+            time_h = '0' + dd.getHours();
+        }
+
+        if (time_m <= 9){
+            time_m = '0' + dd.getMinutes();
+        }
+
+        if (time_s <= 9){
+            time_s = '0' + dd.getSeconds();
+        }
+
+        time_time = time_h + ':' + time_m + ':' + time_s;
+
+        if (date_day <= 9){
+            date_day = '0' + dd.getDate();
+        }
+
+        if (date_month <= 9){
+            date_month = '0' + dd.getMonth();
+        }
+
+        if (date_year <= 9){
+            time_s = '0' + dd.getSeconds();
+        }
+
+
+
+        date_date = date_day  + '/' + (date_month+1)  + '/' + date_year;
+
+
+        
+ 
 
         document.getElementById('time_value').innerHTML = time_time;
         document.getElementById('date_value').innerHTML = date_date;
@@ -63,8 +97,6 @@
             
 
         }
-
-        
 
         
 
